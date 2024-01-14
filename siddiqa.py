3c17f3a1af611dcc7f6e0b3d7a7bd2fbded73637
siddiqasalahuddin <156574435+siddiqasalahuddin@users.noreply.github.com>
2024-01-14 15:10:40 +0500

    Add files via upload

---
+++ siddiqa.py
@@ -0,0 +1,22 @@
+a= int(input("enter your first number"))
+b= int(input("enter your second number"))
+c= input("enter your operations")
+operations = input()
+
+if '+' in c:
+    print(a+b)
+elif '-' in c:
+    print(a-b)
+elif '*' in c:
+    print(a*b)
+elif '/' in c:
+    print(a/b)
+    
+    
+    
+    
+    
+    
+    
+
+    
